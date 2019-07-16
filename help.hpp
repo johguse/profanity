b0df159a580be4c3f3233930eb9d076484177948
Johan Gustafsson <johan@johgu.se>
2019-07-16 22:56:46 +0200

    Fix alignment error in help string

--- help.hpp
+++ help.hpp
@@ -31,7 +31,7 @@ usage: ./profanity [OPTIONS]
 
   Device control:
     -s, --skip <index>      Skip device given by index.
-    -n, --no-cache           Don't load cached pre-compiled version of kernel.
+    -n, --no-cache          Don't load cached pre-compiled version of kernel.
 
   Tweaking:
     -w, --work <size>       Set OpenCL local work size. [default = 64]
